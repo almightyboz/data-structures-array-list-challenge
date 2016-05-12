1feb54f81774a20f413823f197ee99375ecd9c50
Emily <ebosakowski@gmail.com>
2016-05-12 13:25:24 -0500

    Redo release 1

--- array_list.rb
+++ array_list.rb
@@ -21,11 +21,11 @@ class ArrayList
 
   def add(element)
     if has_vacancy?
-      @list[@size] = element
+      @list.set(@size, element)
     else
       @capacity += 1
       new_fixed_array = FixedArray.new(@capacity)
-      new_fixed_array[@capacity] = element
+      new_fixed_array.set(@size, element)
       @list = new_fixed_array
     end
     @size += 1
@@ -34,26 +34,32 @@ class ArrayList
 
   def get(index)
     check_index!(index)
-    if @list[index]
-      return @list[index]
+    if @list.get(index)
+      return @list.get(index)
     else
       raise IndexError
     end
   end
 
-  # can you change an element to null?
+  # can you change an element to nil?
   def set(index, element)
-    check_index!(index)
-    @list[index] = element
-    return element
+    # check_index!(index)
+    if @list.get(index)
+      @list.set(index, element)
+      return element
+    else
+      raise IndexError
+    end
   end
 
   # O is n
   def length
-    @list.each do |item|
-      unless item.empty?
+    to_check = 0
+    if to_check > @capacity
+      if @list.get(to_check)
         @size += 1
       end
+      to_check += 1
     end
     return @size
   end
--- array_list_spec.rb
+++ array_list_spec.rb
@@ -4,16 +4,16 @@ describe ArrayList do
 
   let(:test_list) { ArrayList.new(5) }
 
-  it "instantiates a new object with initial size n" do
+  it "instantiates a new object with initial capacity n" do
     expect(test_list).to be_a(ArrayList)
-    expect(test_list.size).to eq(5)
-    expect(test_list.size).not_to eq(1)
+    expect(test_list.capacity).to eq(5)
+    expect(test_list.capacity).not_to eq(1)
   end
 
   it "adds an element to the end of the list and returns the added element" do
-    expect{ test_list.get(6) }.to raise_error(IndexError)
+    expect{ test_list.get(0) }.to raise_error(IndexError)
     test_list.add("Hello, I am a test.")
-    expect(test_list.get(6)).to eq("Hello, I am a test.")
+    expect(test_list.get(0)).to eq("Hello, I am a test.")
   end
 
   it "returns the added element" do
@@ -22,17 +22,21 @@ describe ArrayList do
 
   it "raises an Out of Bounds/IndexError if asked to return an element that is not in the bounds of the array" do
     expect{ test_list.get(7) }.to raise_error(IndexError)
-    expect(test_list.get(2)).to be_nil
   end
 
   it "returns an item at the specified index" do
-    test_list.set(3, "Hello")
-    expect(test_list.get(3)).to eq("Hello")
+    test_list.add("leaf")
+    test_list.add("flower")
+    expect(test_list.get(0)).to eq("leaf")
+    expect(test_list.get(0)).not_to eq("flower")
+    expect(test_list.get(1)).to eq("flower")
   end
 
   it "sets a value in the array at a specific index and return the element." do
-    expect(test_list.set(2, "shirt")).to eq("shirt")
-    expect(test_list.set(0, "pants")).not_to eq("shirt")
+    test_list.add("leaf")
+    test_list.add("flower")
+    expect(test_list.set(0, "shirt")).to eq("shirt")
+    expect(test_list.get(0)).to eq("shirt")
   end
 
   it "returns an out of bounds error if the index at the set element does not exist" do
@@ -40,20 +44,20 @@ describe ArrayList do
   end
 
   it "returns the length of the list object" do
-    expect(test_list.length).to eq(5)
+    expect(test_list.length).to eq(0)
     test_list.add("addition")
-    expect(test_list.length).to eq(6)
+    expect(test_list.length).to eq(1)
   end
 
-  it "raises an IndexError if there is no element at a specified index" do
-    expect{ test_list.insert(1, "data") }.to raise_error(IndexError)
-  end
+  # it "raises an IndexError if there is no element at a specified index" do
+  #   expect{ test_list.insert(1, "data") }.to raise_error(IndexError)
+  # end
 
-  it "inserts a value at the specified index if it's replacing an existing value" do
-    test_list.set(0, "data")
-    test_list.insert(0, "frog")
-    expect(test_list.get(0)).to eq("frog")
-  end
+  # it "inserts a value at the specified index if it's replacing an existing value" do
+  #   test_list.set(0, "data")
+  #   test_list.insert(0, "frog")
+  #   expect(test_list.get(0)).to eq("frog")
+  # end
 
 
 
